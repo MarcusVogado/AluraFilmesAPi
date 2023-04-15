393157d1fd336c9c0016ae296e6c0af7cc9b762b
marcus vogado <marcus_620@hotmail.com>
2023-04-15 09:58:36 -0300

    Adicionar arquivos de projeto.

---
+++ AluraFilmesAPi/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using AluraFilmesAPi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AluraFilmesAPi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        [HttpPost]
+        [Route("AddFilme")]
+        public IActionResult AdicionarFilme([FromBody] Filme filme)
+        {         
+            
+                filmes.Add(filme);
+                return Ok(filmes);
+           
+        }
+    }
+}
---
+++ AluraFilmesAPi/Models/Filme.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraFilmesAPi.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O título do filme é obrigatório")]
+        [StringLength(60,ErrorMessage ="O título do filme deve ter no máximo 100 caracteres")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+        [StringLength(30,ErrorMessage ="O gênero deve ter no máximo 20 caracteres")]
+        public string Genero { get;set; }
+        
+        [Required]
+        [Range (1,360,ErrorMessage ="Duração deve ter entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Diretor { get; set; }
+
+    }   
+}
